ab22f6f349a8328bda9b6df0c600ae16ef44ca4d
Debmalya Sadhukhan <131870160+deBmalooo@users.noreply.github.com>
2023-10-29 00:01:55 +0530

    lol

--- twod_array.java
+++ twod_array.java
@@ -1,7 +1,37 @@
+import java.util.*;
+
 public class twod_array {
-
-    
-    public static void main(String args[]){
-
+    public static void display(int matrix[][]){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-}
+    public static void search(int matrix[][],int key){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==key){
+                    System.out.println("The key is found at ("+i+","+j+")");
+                }
+                else{
+                    System.out.println("Key not found");
+                }
+            }
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int matrix[][]=new int[3][3];
+        System.out.println("Enter the values");
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+        //display(matrix);
+    search(matrix, 8);
+    }
+}  
+
