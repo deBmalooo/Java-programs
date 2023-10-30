79667d816060641cac6dcebe03e938d71d003b03
Debmalya Sadhukhan <131870160+deBmalooo@users.noreply.github.com>
2023-10-30 10:52:11 +0530

    lol

--- recursion.java
+++ recursion.java
@@ -1,8 +1,24 @@
+import java.text.BreakIterator;
+
 public class recursion {
-    public static int dec_num(int n){
-        
+    public static void dec_num(int n){
+       if(n==1){
+        System.out.print(1);
+        return; //After this statement -1 will not come or can say function after this will be returned
+       }
+       System.out.print(n+" ");
+       dec_num(n-1);
+    }
+    public static void inc_num(int n, int i){
+        if(i==n){
+            System.out.print(n);
+            return;
+        }
+        System.out.print(i++ + " ");
+        inc_num(n, i);
     }
     public static void main(String args[]){
-        dec_num(5);
+        //dec_num(5);
+        inc_num(5,1);
     }
 }
