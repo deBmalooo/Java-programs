9ba10d763f4547e4f4bcd6c3d9ce221b71f4c4fe
Debmalya Sadhukhan <131870160+deBmalooo@users.noreply.github.com>
2023-12-02 10:59:30 +0530

    idk

--- divide_con.java
+++ divide_con.java
@@ -41,11 +41,34 @@ public class divide_con {
         merge(arr, si,mid,ei);
     }
     public static void quicksort(int arr[],int si,int ei){
-        
+        if(si>=ei){
+            return;
+        }
+        int pidx=partition(arr,si,ei);
+        quicksort(arr, si, pidx-1);
+        quicksort(arr, pidx-1, ei);
+    }
+    public static int partition(int arr[],int si,int ei){
+        int pivot=arr[ei];
+        int i=si-1;
+        for(int j=si;j<ei;j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        i++;
+        int temp=pivot;
+        arr[ei]=arr[i];
+        arr[i]=temp;
+        return i;
     }
     public static void main(String args[]){
         int arr[]={6,3,9,5,2,8};
-        mergesort(arr, 0, arr.length-1);
+        //mergesort(arr, 0, arr.length-1);
+        quicksort(arr, 0, arr.length-1);
         printarr(arr);
     }
 }
